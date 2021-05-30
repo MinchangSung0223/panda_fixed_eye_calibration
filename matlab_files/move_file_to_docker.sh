6f581a7a4308000a58d4a6eee4a7e1873ef716ba
tjdalsckd <53217819+tjdalsckd@users.noreply.github.com>
2021-05-31 00:34:35 +0900

    Create move_file_to_docker.sh

---
+++ matlab_files/move_file_to_docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker cp Images calibration_smc:/root/workspace/calibration_application/application/
+docker cp Poses calibration_smc:/root/workspace/calibration_application/application/
